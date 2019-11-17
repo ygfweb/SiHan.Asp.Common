8c80e3baf9d6352652e7668232bb5af23e017e60
ygfweb <ygfweb@hotmail.com>
2019-11-18 02:38:11 +0800

    update to 0.0.4-alpha

---
+++ SiHan.Asp.Common/SiHan.Asp.Common/Extensions/HttpRequestExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SiHan.Asp.Common.Extensions
+{
+    /// <summary>
+    /// HttpRequest扩展
+    /// </summary>
+    public static class HttpRequestExtensions
+    {
+        private const string RequestedWithHeader = "X-Requested-With";
+        private const string XmlHttpRequest = "XMLHttpRequest";
+
+        /// <summary>
+        /// 判断是否是AJAX请求
+        /// </summary>
+        public static bool IsAjaxRequest(this HttpRequest request)
+        {
+            if (request == null)
+            {
+                throw new ArgumentNullException("request");
+            }
+
+            if (request.Headers != null)
+            {
+                return request.Headers[RequestedWithHeader] == XmlHttpRequest;
+            }
+
+            return false;
+        }
+    }
+}
